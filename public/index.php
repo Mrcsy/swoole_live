cc6d1fa66c83b857cd3efb7c1b24e3111ebe0bbf
chengshuo <554029142@qq.com>
2020-08-06 15:54:46 +0800

    验证swoole

--- public/index.php
+++ public/index.php
@@ -1,21 +1,20 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
-// [ 应用入口文件 ]
-namespace think;
 
-// 加载基础文件
-require __DIR__ . '/../thinkphp/base.php';
+// 表明服务器启动后监听本地 9051 端口
+$server = new swoole_http_server('127.0.0.1', 9501);
 
-// 支持事先使用静态方法设置Request对象和Config对象
+// 服务器启动时返回响应
+$server->on("start", function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+});
 
-// 执行应用并响应
-Container::get('app')->run()->send();
+// 向服务器发送请求时返回响应
+// 可以获取请求参数，也可以设置响应头和响应内容
+$server->on("request", function ($request, $response) {
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World\n");
+});
+
+// 启动 HTTP 服务器
+$server->start();
